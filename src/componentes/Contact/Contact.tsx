421bcf5a1be5b81c5ddd1d28bd072344e9fecba1
mcarocortes <m.caro.cortes2@gmail.com>
2026-08-18 20:38:43 +0200

    Contact better

--- src/componentes/Contact/Contact.tsx
+++ src/componentes/Contact/Contact.tsx
@@ -35,9 +35,7 @@ export default function Contact() {
                             <span>{t("contactSection.madeWithHighlight")}</span>
                             {t("contactSection.madeWithAfter")}
                         </p>
-                    </div>
-
-                    <div className="items">
+                                            <div className="items">
                         <ul>
                             <li>
                                 <a href="mailto:m.caro.cortes2@gmail.com" className="list">
@@ -79,12 +77,56 @@ export default function Contact() {
                             </li>
                         </ul>
                     </div>
+                    </div>
+
+                    {/*<div className="items">
+                        <ul>
+                            <li>
+                                <a href="mailto:m.caro.cortes2@gmail.com" className="list">
+                                    {t("contactSection.email")}
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    href="https://www.linkedin.com/in/macarena-caro-cortes/"
+                                    className="list"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    LinkedIn
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    href="https://github.com/mcarocortes"
+                                    className="list"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    GitHub
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    href="https://www.instagram.com/macarocortes/"
+                                    className="list"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Instagram
+                                </a>
+                            </li>
+                        </ul>
+                    </div>*/}
                 </div>
 
                 <div className="logoTrack"></div>
 
                 <div className="footerLine">
-                    {t("contactSection.copyright")}
+                    <p>{t("contactSection.copyright")}</p>
                     <Link
                         to="#Home"
                         className="back"
